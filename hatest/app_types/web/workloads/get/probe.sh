fd5599882479c6208081c4fedfcc75959585e9f4
RichardExley <Richard.exley@oracle.com>
2022-09-13 10:31:44 -0700

    log

--- hatest/app_types/web/workloads/get/probe.sh
+++ hatest/app_types/web/workloads/get/probe.sh
@@ -1,4 +1,4 @@
-hatest_result_app_outage client_send get_1
+hatest_result_app_outage 'client_send get_1'
 # wget http://127.0.0.1:8080/user/1
 sleep 0.05
-hatest_result_app_outage client_response get_1 code
+hatest_result_app_outage 'client_response get_1 code'
--- hatest/app_types/web/workloads/get/start.sh
+++ hatest/app_types/web/workloads/get/start.sh
@@ -1,5 +1,5 @@
 count="60"
-spacing="1" #seconds
+spacing="1.0" #seconds
 
 # Each probe is run in a separate thread
 for (( p=1; p<=$count; p++))
