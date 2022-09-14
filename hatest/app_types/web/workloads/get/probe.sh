28cfa336985fcc2be4959240f4c47ca8259325b8
RichardExley <Richard.exley@oracle.com>
2022-09-14 15:00:59 -0700

    Update probe.sh

--- hatest/app_types/web/workloads/get/probe.sh
+++ hatest/app_types/web/workloads/get/probe.sh
@@ -2,10 +2,10 @@ probe_number="$1"
 hatest_result_app_outage "client_send $probe_number get_1"
 HATEST_APP_URL='http://127.0.0.1:8080/user/1'
 # get output, append HTTP status code in separate line, discard error message
-OUT=$( curl --max-time 5 -qSfsw '\n%{total_time}\n%{http_code}' "${HATEST_APP_URL}?probe=${probe_number}" ) 2>/dev/null
+OUT=$( curl --max-time 5 -qSfsw '\n%{time_total} %{http_code}' "${HATEST_APP_URL}?probe=${probe_number}" ) 2>/dev/null
 
 # get exit code
 RET=$?
 
-status="$(echo "$OUT" | tail -n2 | head -n1 ) $(echo "$OUT" | tail -n1 ) $RET"
+status="$(echo "$OUT" | tail -n1 ) $RET"
 hatest_result_app_outage "client_response $probe_number $status"
