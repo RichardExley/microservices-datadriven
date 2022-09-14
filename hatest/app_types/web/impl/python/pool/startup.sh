2848b57122dd49dcf9cde7c2c76ea413ba50f89d
RichardExley <Richard.exley@oracle.com>
2022-09-14 14:51:37 -0700

    total time

--- hatest/app_types/web/impl/python/pool/startup.sh
+++ hatest/app_types/web/impl/python/pool/startup.sh
@@ -6,4 +6,5 @@ export PYO_SAMPLES_CONNECT_STRING="$HATEST_DB_CONNECT_STRING"
 export PYO_SAMPLES_DRIVER_MODE=thick
 python3 app.py &
 echo "$!" > ~/.web_python.pid
+sleep 10
 hatest_result_app_outage startup_end
--- hatest/app_types/web/workloads/get/probe.sh
+++ hatest/app_types/web/workloads/get/probe.sh
@@ -2,16 +2,10 @@ probe_number="$1"
 hatest_result_app_outage "client_send $probe_number get_1"
 HATEST_APP_URL='http://127.0.0.1:8080/user/1'
 # get output, append HTTP status code in separate line, discard error message
-OUT=$( curl --max-time 5 -qSfsw '\n%{http_code}' "${HATEST_APP_URL}?probe=${probe_number}" ) 2>/dev/null
+OUT=$( curl --max-time 5 -qSfsw '\n%{total_time}\n%{http_code}' "${HATEST_APP_URL}?probe=${probe_number}" ) 2>/dev/null
 
 # get exit code
 RET=$?
 
-if [[ $RET -ne 0 ]] ; then
-    # print HTTP error
-    status="$(echo "$OUT" | tail -n1 ), return code $RET"
-else
-    # otherwise print last line of output, i.e. HTTP status code
-    status="$(echo "$OUT" | tail -n1 )"
-fi
-hatest_result_app_outage "client_response $probe_number get_1 $status"
+status="$(echo "$OUT" | head -n1 ) $(echo "$OUT" | tail -n1 ) $RET"
+hatest_result_app_outage "client_response $probe_number $status"
