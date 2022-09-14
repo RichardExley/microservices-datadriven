d1b04deab329c54cf6ad15a28bb27df52d88f179
RichardExley <Richard.exley@oracle.com>
2022-09-14 15:29:25 -0700

    basic

---
+++ hatest/app_types/web/impl/python/basic/reset.sh
@@ -0,0 +1,6 @@
+hatest_result_app_outage reset_start
+if test -f ~/.web_python.pid; then
+  kill -9 "$(<~/.web_python.pid)"
+  rm ~/.web_python.pid
+fi
+hatest_result_app_outage reset_end
---
+++ hatest/app_types/web/impl/python/basic/startup.sh
@@ -0,0 +1,10 @@
+batch_name="$1"
+hatest_result_app_outage startup_start
+export PYO_SAMPLES_MAIN_USER="$HATEST_DB_MAIN_USER"
+export PYO_SAMPLES_MAIN_PASSWORD="$HATEST_DB_MAIN_PASSWORD"
+export PYO_SAMPLES_CONNECT_STRING="$HATEST_DB_CONNECT_STRING"
+export PYO_SAMPLES_DRIVER_MODE=thick
+python3 app.py &
+echo "$!" > ~/.web_python.pid
+sleep 10
+hatest_result_app_outage startup_end
