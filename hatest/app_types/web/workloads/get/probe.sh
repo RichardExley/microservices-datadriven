e36aa9811237bc30ce017ee08f3d94001a615646
RichardExley <Richard.exley@oracle.com>
2022-09-14 14:53:30 -0700

    Update probe.sh

--- hatest/app_types/web/workloads/get/probe.sh
+++ hatest/app_types/web/workloads/get/probe.sh
@@ -7,5 +7,5 @@ OUT=$( curl --max-time 5 -qSfsw '\n%{total_time}\n%{http_code}' "${HATEST_APP_UR
 # get exit code
 RET=$?
 
-status="$(echo "$OUT" | head -n1 ) $(echo "$OUT" | tail -n1 ) $RET"
+status="$(echo "$OUT" | tail -n2 | head -n1 ) $(echo "$OUT" | tail -n1 ) $RET"
 hatest_result_app_outage "client_response $probe_number $status"
