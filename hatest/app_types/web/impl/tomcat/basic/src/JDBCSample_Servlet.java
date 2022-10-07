8bfafb9f1f7322a3c9379556ddd5a6692d773992
RichardExley <Richard.exley@oracle.com>
2022-10-07 10:16:42 -0700

    Update JDBCSample_Servlet.java

--- hatest/app_types/web/impl/tomcat/basic/src/JDBCSample_Servlet.java
+++ hatest/app_types/web/impl/tomcat/basic/src/JDBCSample_Servlet.java
@@ -19,7 +19,7 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * Servlet implementation class JDBCSample_Servlet
  */
-c("/user/*")
+@WebServlet("/user/*")
 public class JDBCSample_Servlet extends HttpServlet {
   private static final long serialVersionUID = 1L;     
     /**
