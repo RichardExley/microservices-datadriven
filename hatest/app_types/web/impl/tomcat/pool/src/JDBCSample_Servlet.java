0697168e86fa9f8b7cd52fe43fca4babb2563690
RichardExley <Richard.exley@oracle.com>
2022-10-17 16:00:45 -0700

    pool2

--- hatest/app_types/web/impl/tomcat/pool/src/JDBCSample_Servlet.java
+++ hatest/app_types/web/impl/tomcat/pool/src/JDBCSample_Servlet.java
@@ -5,7 +5,6 @@ import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.PreparedStatement;
-//import java.sql.DriverManager;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
