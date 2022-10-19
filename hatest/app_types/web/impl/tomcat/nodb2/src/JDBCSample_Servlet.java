7912d43c4927a23350012c5688c8ea28035208fb
RichardExley <Richard.exley@oracle.com>
2022-10-19 09:07:16 -0700

    nodb2

---
+++ hatest/app_types/web/impl/tomcat/nodb2/src/JDBCSample_Servlet.java
@@ -0,0 +1,39 @@
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/*")
+public class JDBCSample_Servlet extends HttpServlet {
+  private static final long serialVersionUID = 1L;     
+
+  public JDBCSample_Servlet() {
+    super();
+  }
+
+  protected void doGet(HttpServletRequest request, HttpServletResponse response)
+           throws ServletException, IOException {
+    try {
+      String id = request.getPathInfo().split("/")[1];
+      String probe = httpRequest.getParameter("probe");
+      System.out.println("doGet: id: " + id + " probe: " + probe);
+
+      if (id.equals("1")) {
+        response.setStatus(200);
+        response.getWriter().println("chris");
+      } else {
+        response.setStatus(201);
+      }
+    } catch (Exception e) {
+      response.setStatus(500);
+      response.setHeader("Exception", e.toString());
+      e.printStackTrace();
+    }
+  }
+
+  public void destroy() { }
+}
