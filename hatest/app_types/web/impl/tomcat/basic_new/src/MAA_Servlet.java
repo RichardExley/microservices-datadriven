d71906c4a7617387a7b6538f6dc2d73a05e9abbb
RichardExley <Richard.exley@oracle.com>
2022-10-19 16:24:38 -0700

    tidy

--- hatest/app_types/web/impl/tomcat/basic_new/src/MAA_Servlet.java
+++ hatest/app_types/web/impl/tomcat/basic_new/src/MAA_Servlet.java
@@ -5,16 +5,29 @@ import java.io.PrintWriter;
 
 import java.util.logging.*;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.PreparedStatement;
+import java.sql.DriverManager;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
 // MAA Tip: Load on Startup
 @WebServlet(name="MAA_Servlet", urlPatterns="/*", loadOnStartup=1)
 public class MAA_Servlet extends HttpServlet {
   private static final long serialVersionUID = 1L;     
   private static Logger logger = Logger.getLogger("com.oracle.maa.tomcat.maa_servlet");
+
+  static String get_id_sql = "select username from demo where id = ?"; 
+
+  static String dbPassword = System.getenv("HATEST_DB_MAIN_PASSWORD");
+  static String dbUser     = System.getenv("HATEST_DB_MAIN_USER");
+  static String dbURL      = System.getenv("HATEST_JDBC_URL");
 
   public MAA_Servlet() {
     super();
@@ -23,6 +36,13 @@ public class MAA_Servlet extends HttpServlet {
   public void init() {
     logger.setLevel(Level.ALL);
     logger.info("Servlet starting");
+
+    // Load JDBC driver
+    try {
+      DriverManager.registerDriver (new oracle.jdbc.OracleDriver());
+    } catch (SQLException e) {
+      logger.log(Level.SEVERE, "Exception loading the JDBC driver", e);
+    }
   }
   
   protected void doGet(HttpServletRequest request, HttpServletResponse response)
@@ -30,21 +50,24 @@ public class MAA_Servlet extends HttpServlet {
     String id = "";
     String probe = "";
 
-    try {
+    try (Connection conn = DriverManager.getConnection(dbURL, dbUser, dbPassword)) {
       id = request.getPathInfo().split("/")[1];
       probe = request.getParameter("probe");
       logger.info("id: " + id + " probe: " + probe);
 
-      if (id.equals("1")) {
+      PreparedStatement stmt = conn.prepareStatement(get_id_sql); 
+      stmt.setString(1, id);
+      ResultSet rs = stmt.executeQuery(); 
+      if (rs.next()) {
         response.setStatus(200);
-        response.getWriter().println("chris");
+        out.println(rs.getString(1));
       } else {
         response.setStatus(201);
       }
     } catch (Exception e) {
       response.setStatus(500);
       response.setHeader("Exception", e.toString());
-      logger.log(Level.SEVERE, "An exception was thrown processing probe# " + probe, e);
+      logger.log(Level.SEVERE, "Exception processing probe# " + probe, e);
     }
   }
 
