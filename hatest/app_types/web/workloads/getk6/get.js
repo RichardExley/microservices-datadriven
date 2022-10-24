2a2eb3d5d3776b21bc557d3e0c3495ce0ec4615b
RichardExley <Richard.exley@oracle.com>
2022-10-24 14:11:22 -0700

    Update get.js

--- hatest/app_types/web/workloads/getk6/get.js
+++ hatest/app_types/web/workloads/getk6/get.js
@@ -28,5 +28,5 @@ export const options = {
 };
 
 export default function () {
-  http.get('https://localhost:8080/user/1?probe=' + __ITER.toString());
+  http.get('http://localhost:8080/user/1?probe=' + __ITER.toString());
 }
