2f183e68a0e62aef6bd5236a364770f14ecb65f7
RichardExley <Richard.exley@oracle.com>
2022-11-14 09:12:01 -0800

    Update install.sh

--- hatest/app_types/web/install.sh
+++ hatest/app_types/web/install.sh
@@ -2,7 +2,7 @@
 # Copyright (c) 2023 Oracle and/or its affiliates.
 # Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl.
 
-cd ~/hatest/microservices-datadriven/app_types/web/impl/tomcat
+cd ~/hatest/microservices-datadriven/hatest/app_types/web/impl/tomcat
 ./install.sh 
-cd ~/hatest/microservices-datadriven/app_types/web/impl/python
+cd ~/hatest/microservices-datadriven/hatest/app_types/web/impl/python
 ./install.sh 
